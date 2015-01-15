757cf8b11b3c9d81e666c02558e83ad0284dec4e
skootdiggity <smmiller.work@gmail.com>
2015-01-14 17:17:12 -0700

    favorite things

--- objectReview.js
+++ objectReview.js
@@ -1,26 +1,45 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
+// Have the values to those keys be your favorite thing in that category. 
 
   //Your code here
+var favoriteThings = {
+  band: 'Roxette',
+  food: 'Lasagna',
+  person: 'Ash',
+  book: 'BoM',
+  movie: 'Twister',
+  holiday: 'Christmas'
+};
 
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+
+//After you've made your object, add another key named 'car' with the value being your favorite car and
+// then another key named 'brand' with the value being your favorite brand.
 
   //Your code here
+favoriteThings.car = "Tacoma";
+favoriteThings.brand = "Toyota";
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your
+// favoriteThings object to be '50 Shades of Gray'. 
 
   //Your code here
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
+
 
 //Now, alert your favorite person, then alert your favorite book.
 
   //Your code here
-
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-var user = {a
+var user = {
     name: 'Tyler McGinnis', 
     email: null,
     pwHash: 'U+Ldlngx2BYQk',
@@ -29,13 +48,19 @@ var user = {a
     age: 0
 }
 
-//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
+//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy.
+//If it's not truthy, remove it from the object. hint: 'delete'.
 
   //Your code here
 
-//Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
+
+
+//Once you get your truthy object, Change the remaining values in the object to be specific to you
+// (name: 'your name', username: 'your username'), rather than my information.
 
   //Your code here
+
+
 
 //Now console.log your object and make sure it looks right.
 
@@ -51,9 +76,14 @@ var user = {a
 
   //Your code here
 
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
+
+
+//Now add two methods (functions that are properties on objects) to your methodCollection object.
+// One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
   //Your code here
+
+
 
 //Now call your alertHello and logHello methods. 
 
@@ -65,7 +95,8 @@ var user = {a
 
 
 
-//write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
+//write a function called voweler that accepts a string, and returns an object with the keys being all
+// the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
 
 
